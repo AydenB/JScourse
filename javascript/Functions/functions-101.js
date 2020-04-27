ece9b18c7612fa7c433127207ba360cd7f94708c
AydenB <ayden@cdrsoftware.com>
2020-04-26 22:00:00 -0500

    housekeeping- remove line

---
+++ javascript/Functions/functions-101.js
@@ -0,0 +1,35 @@
+// Function - input (argument), code, output
+
+let greetUser = function(){
+    console.log('Welcome User')
+}
+
+greetUser()
+greetUser()
+
+let square = function(num){
+    let result = num * num
+    return result
+}
+
+let value = square(3)
+
+let otherValue = square(10)
+console.log(value)
+console.log(otherValue)
+
+//challenge
+
+// convert Fahrenheit to Celsius
+
+//(32-0)
+//print the converted values
+
+let convert = function(temp){
+    let resrult = (temp - 32) * 5/9
+    return esult
+}
+
+let value1 = convert(32)
+
+console.log(value1)
