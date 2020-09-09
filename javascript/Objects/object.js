80f98494a35fed258bb6b70a7014394776ec92a9
butera.ayden@gmail.com <ayden@pop-os.localdomain>
2020-09-08 22:42:27 -0500

    added comments

--- javascript/Objects/object.js
+++ javascript/Objects/object.js
@@ -1,4 +1,4 @@
-Objects allow us to store related info in a single place. This is useful when dealing with books, users, etc. we can store properties of such. Book- title, author, pages.
+//Objects allow us to store related info in a single place. This is useful when dealing with books, users, etc. we can store properties of such. Book- title, author, pages.
 
 let myBook = {
 	title: 'Breakfast of Champions'
