c41624ddf47e95d829d46f3d5e454ad31850f8be
butera.ayden@gmail.com <ayden@pop-os.localdomain>
2020-09-09 21:59:33 -0500

    created book object

---
+++ javascript/Objects/objects-101.js
@@ -0,0 +1,18 @@
+let myBook = {
+    title: '1984',
+    author: 'George Orwell',
+    pageCount: 326
+}
+
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+// curly braces signify an object
+//properties are pieces of data for an object
+// you can use . (dot notation) to get properties
