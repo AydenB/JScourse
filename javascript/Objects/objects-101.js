ed9b154f21a1d5c6996522e0380c16bf5a409f1c
butera.ayden@gmail.com <butera.ayden@gmail.com>
2020-09-09 22:42:48 -0500

    created book object

--- javascript/Objects/objects-101.js
+++ javascript/Objects/objects-101.js
@@ -16,3 +16,16 @@ console.log(`${myBook.title} by ${myBook.author}`)
 // curly braces signify an object
 //properties are pieces of data for an object
 // you can use . (dot notation) to get properties
+
+// Ayden is 32 and lives in Louisiana
+//increase age by 1 and print again
+let myLife = {
+    age: 32,
+    Name: "Ayden",
+    Location: "Louisiana"
+}
+
+console.log(`${myLife.Name} is ${myLife.age} and lives in ${myLife.Location}`)
+myLife.age = myLife.age + 1
+console.log(`${myLife.Name} is ${myLife.age} and lives in ${myLife.Location}`)
+
